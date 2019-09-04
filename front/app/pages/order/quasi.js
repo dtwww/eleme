1f863a726984404435081c948eac095e448cef66
Astory <dracomalfoy233@163.com>
2019-09-04 15:40:47 +0800

    delete-quasi

--- front/app/pages/order/quasi.js
+++ front/app/pages/order/quasi.js
@@ -1,7 +1,4 @@
 // pages/order/quasi.js
-import {
-  updateOrderAddr
-} from '../../utils/apis'
 Page({
   data: {
     content: ''
@@ -20,41 +17,6 @@ Page({
   },
   onUnload: function () {
     // 页面关闭
-  },
-  callbackAddress(addr_id) {
-    var that = this
-    var {id} = this
-    var {loading} = this.data
-    if (loading) {
-      return
-    }
-    this.setData({
-      loading: true
-    })
-    // wx.showNavigationBarLoading()
-    updateOrderAddr({
-      quasi_order_id: id,
-      addr_id,
-      success(data) {
-        data['cut_money_total'] = +data.cut_money + +data.coupon_money
-        that.setData({
-          info: data,
-          loading: false
-        })
-        wx.hideNavigationBarLoading()
-      },
-      error() {
-        that.setData({
-          loading: false
-        })
-        wx.hideNavigationBarLoading()
-      }
-    })
-  },
-  callbackContent(content) {
-    this.setData({
-      content
-    })
   },
   onAddOrder(e) {
     // console.log(e)
